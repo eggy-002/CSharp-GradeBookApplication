9e400b40fa59353ba3ca664da8f98b8940d05f56
Elgardo Echevarria <elgardoe@gmail.com>
2021-03-19 14:28:07 -0400

    completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,8 +38,20 @@ namespace GradeBook.GradeBooks
          {
             base.CalculateStatistics();
          }
+      }
 
-
+      public override void CalculateStudentStatistics(string name)
+      {
+         if (Students.Count < 5)
+         {
+            Console.WriteLine
+               ("Ranked grading requires at lease 5 students with grades in" +
+               "order to properly calculate a student's overall grade.");
+         }
+         else
+         {
+            base.CalculateStudentStatistics(name);
+         }
       }
    }
 }
